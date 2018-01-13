7b9c3a075c016195299e32071b9cd15c41d8f175
totoff974 <totoff974@gmail.com>
2018-01-13 20:00:57 +0400

    beta->stable

--- resources/JeePlcBusd/JeePlcBusd.py
+++ resources/JeePlcBusd/JeePlcBusd.py
@@ -69,42 +69,192 @@ plcbus_command_to_hex = {
 			}
 
 plcbus_hex_to_command = {
-			0x00 : 'ALL_UNITS_OFF',
-			0x01 : 'ALL_LIGHTS_ON',
-			0x02 : 'ON',
-			0x03 : 'OFF',
-			0x04 : 'DIM',
-			0x05 : 'BRIGHT',
-			0x06 : 'ALL_LIGHTS_OFF',
-			0x07 : 'ALL_USER_LIGHTS_ON',
-			0x08 : 'ALL_USER_UNITS_OFF',
-			0x09 : 'ALL_USER_LIGHTS_OFF',
-			0x0a : 'BLINK',
-			0x0b : 'FADE_STOP',
-			0x0c : 'PRESET_DIM',
-			#0x0d : 'STATUS_ON',
-			#0x0e : 'STATUS_OFF',
-			0x0f : 'STATUS_REQUEST',
-			0x10 : 'RX_MASTER_ADDR_SETUP',
-			0x11 : 'TX_MASTER_ADDR_SETUP',
-			0x12 : 'SCENE_ADDR_SETUP',
-			0x13 : 'SCENE_ADDR_ERASE',
-			0x14 : 'ALL_SCENES_ADDR_ERASE',
-			0x18 : 'GET_SIGNAL_STRENGTH',
-			0x19 : 'GET_NOISE_STRENGTH',
-			#0x1a : 'REPORT_SIGNAL_STRENGTH',
-			#0x1b : 'REPORT_NOISE_STRENGTH',
-			0x1c : 'GET_ALL_ID_PULSE',
-			0x1d : 'GET_ON_ID_PULSE'
+			'0x0' : 'ALL_UNITS_OFF',
+			'0x1' : 'ALL_LIGHTS_ON',
+			'0x2' : 'ON',
+			'0x3' : 'OFF',
+			'0x4' : 'DIM',
+			'0x5' : 'BRIGHT',
+			'0x6' : 'ALL_LIGHTS_OFF',
+			'0x7' : 'ALL_USER_LIGHTS_ON',
+			'0x8' : 'ALL_USER_UNITS_OFF',
+			'0x9' : 'ALL_USER_LIGHTS_OFF',
+			'0xa' : 'BLINK',
+			'0xb' : 'FADE_STOP',
+			'0xc' : 'PRESET_DIM',
+			'0xd' : 'STATUS_ON',
+			'0xe' : 'STATUS_OFF',
+			'0xf' : 'STATUS_REQUEST',
+			'0x10' : 'RX_MASTER_ADDR_SETUP',
+			'0x11' : 'TX_MASTER_ADDR_SETUP',
+			'0x12' : 'SCENE_ADDR_SETUP',
+			'0x13' : 'SCENE_ADDR_ERASE',
+			'0x14' : 'ALL_SCENES_ADDR_ERASE',
+			'0x18' : 'GET_SIGNAL_STRENGTH',
+			'0x19' : 'GET_NOISE_STRENGTH',
+			'0x1a' : 'REPORT_SIGNAL_STRENGTH',
+			'0x1b' : 'REPORT_NOISE_STRENGTH',
+			'0x1c' : 'GET_ALL_ID_PULSE',
+			'0x1d' : 'GET_ON_ID_PULSE'
 			}
 			
+
 # ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+
+###########################
+## lecture des trames recues
+###########################
+def MiseEnFormeReceive(trame_rx, format):
+	trame = []
+	
+	for i in range(0, len(trame_rx), 2):
+		if (format == "str"):
+			trame.append(trame_rx[i:i+2])
+		elif (format == "int"):
+			trame.append(int(trame_rx[i:i+2],16))
+		else:
+			trame = []
+			
+	return trame
+
+def Valid_RX_Receive(trame_rx):
+	trame_int = MiseEnFormeReceive(trame_rx, 'int')
+	
+	checksum = sum(trame_int)
+	
+	if (((trame_int[0] == 2) and (trame_int[1] == 6) and (trame_int[8] == 3)) or (checksum == 512)):
+		return True
+	else:
+		return False
+		
+def Statut_RX_Receive(homeunit, action, trame_rx):
+	trame_int = MiseEnFormeReceive(trame_rx, 'int')
+	statut = trame_int[4] & 31
+	
+	if (homeunit and homeunit == trame_int[3]):
+		# si 3 phases
+		if (_phase == 3):
+			if (trame[4] == 13 or trame[4] == 14 or trame[4] >= 30):
+				return True
+		
+		elif (action == 28 or action == 29):
+			if (trame_int[7] == 12):
+				return True
+		
+		elif (action == 18 or action == 19 or action == 20 or ((action == 2 or action == 3) and ((trame_int[3] & 15) >= 9))):
+			if (trame_int[7] == 28):
+				return True
+				
+		elif (trame_int[7] == 32):
+			return True
+	
+	elif (statut == 13 or statut == 14 or (homeunit and homeunit != trame_int[3])):
+		logging.debug("Trafic autre controleur")
+		logging.debug(plcbus_rx_out(trame_rx))
+		return True
+		
+	elif not homeunit:
+		logging.debug(plcbus_rx_out(trame_rx))
+		return True
+		
+	else:
+		return False
+
+def plcbus_rx_out(trame_rx):
+	trame_int = MiseEnFormeReceive(trame_rx, 'int')
+	
+	# pour la commande
+	commande = trame_int[4] & 31
+	plcbus_cmd = plcbus_hex_to_command[hex(commande)]
+
+	# pour le homeunit
+	home = (trame_int[3] / 16) + 65
+	home = struct.pack('l', home)
+	unit = trame_int[3] %16 + 1
+	homeunit = str(home) + str(unit)
+	
+	# pour les data1 et data2
+	data1 = str(trame_int[5])
+	data2 = str(trame_int[6])
+	
+	plcbus_receive = homeunit + "::" + plcbus_cmd + "::" + data1 + "::" + data2
+	return plcbus_receive
+	
+def affiche_rx(homeunit, action, trame_rx, num_ack):
+	if Valid_RX_Receive(trame_rx):
+		if Statut_RX_Receive(homeunit, action, trame_rx):
+			send_socket(plcbus_rx_out(trame_rx), num_ack)
+	
+###########################
+## Mise en forme des trames a envoyer
+###########################
+
+def MiseEnFormeCommande(CommandeTexte, ack):
+	# commande_texte -> exemple -> A1::ON::0:0
+	params = str(CommandeTexte)
+	params_data = params.split("::")
+
+	### traitement du homeunit
+	homeunit = params_data[0]
+	home = params_data[0][:1]
+	unit = params_data[0][1:]
+	
+	home = (int(home.encode("hex"), 16) - 65) * 16
+	unit = int(unit) - 1
+	homeunit = (hex(home + unit)[2:].upper()).zfill(2) # en str mise en forme 2 digits
+	
+	### traitement de la commande
+	command = int(plcbus_command_to_hex[params_data[1]]) # en decimale
+	
+	# bit-5 a 1 pour ACK_pulse
+	if (command != 28 or command != 29) and ack != "0":
+		command += 32
+		
+	# bit-6 a 1 si 3 phases
+	if _phase == 3:
+		command += 64
+		
+	command = (hex(command)[2:].upper()).zfill(2) # en str mise en forme 2 digits
+	
+	## traitement plcbus_data1 et plcbus_data2
+	
+	try:
+		plcbus_data1 = params_data[2]
+	except:
+		plcbus_data1 = "00"	
+	
+	try:
+		plcbus_data2 = params_data[3]
+	except:
+		plcbus_data2 = "00"	
+	
+	if ((int(plcbus_data1) < 0) or (int(plcbus_data1) > 100)):
+		logging.debug("ERREUR, Data1 Value")
+		return 0
+
+	if ((int(plcbus_data2) < 0) or (int(plcbus_data2) > 100)):
+		logging.debug("ERREUR, Data2 Value")
+		return 0
+	
+	plcbus_data1 = (hex(int(plcbus_data1))[2:].upper()).zfill(2) # en str mise en forme 2 digits
+	plcbus_data2 = (hex(int(plcbus_data2))[2:].upper()).zfill(2) # en str mise en forme 2 digits
+	
+	## frame a envoyer
+	plcbus_frame = "02" + "05" + _usercode + homeunit + command + plcbus_data1 + plcbus_data2 + "03"
+	return plcbus_frame
+
+# ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+
+
 
 def read_socket():
 	try:
 		global JEEDOM_SOCKET_MESSAGE
 		if not JEEDOM_SOCKET_MESSAGE.empty():
-			logging.debug("Message received in socket JEEDOM_SOCKET_MESSAGE")
 			message = json.loads(jeedom_utils.stripped(JEEDOM_SOCKET_MESSAGE.get()))
 			if message['apikey'] != _apikey:
 				logging.error("Invalid apikey from socket : " + str(message))
@@ -113,232 +263,52 @@ def read_socket():
 					logging.error("Invalid apikey from socket : " + str(message))
 					return
 			elif message['cmd'] == 'send':
+				ack = message['ack']
+				logging.debug("ACK => " + ack)
 				if isinstance(message['data'], list):
 					for data in message['data']:
 						try:
-							send_plcbus(data)
+							send_plcbus(data, ack)
 						except Exception, e:
 							logging.error('Send command to PlcBus error : '+str(e))
-				else:
-					try:
-						send_plcbus(message['data'])
-					except Exception, e:
-						logging.error('Send command to PlcBus error : '+str(e))
 	except Exception,e:
 		logging.error('Error on read socket : '+str(e))
 
 # ----------------------------------------------------------------------------	
 
-def send_plcbus(message):
-	message = test_PlcBus(message)
+def send_plcbus(plcbus_frame, ack):
 	jeedom_serial.flushOutput()
 	jeedom_serial.flushInput()
-	logging.debug("Write message to serial port")
-	logging.debug("WRITE : " + message)
-	jeedom_serial.write(message.decode('hex'))
+	plcbus_frame = MiseEnFormeCommande(plcbus_frame, ack)
+	logging.debug("Write Serial Port : " + plcbus_frame)
+	jeedom_serial.write(plcbus_frame.decode('hex'))
 	time.sleep(3)
-	logging.debug("Write message ok : "+ jeedom_utils.ByteToHex(message.decode('hex')))
-	try:
-		logging.debug("Decode message")
-	except Exception, e:
-		logging.error('Unrecognizable packet : '+str(e))
+	logging.debug("Write message Ok  : " + plcbus_frame)
 
 # ----------------------------------------------------------------------------
 
-def test_PlcBus(message):
-	logging.debug("Test message: " + message)
-	message = plcbus_tx_command(message)
-	message = jeedom_utils.stripped(message)
-	logging.debug("STRIPPED : " + message)
-	try:
-		message = message.replace(' ', '')
-	except Exception, e:
-		logging.debug("Error: Removing white spaces")
-		return False
-	try:
-		int(message,16)
-	except Exception, e:
-		logging.debug("Error: Packet not hex format")
-		return False
-	if len(message) % 2:
-		logging.debug("Error: Packet length not even")
-		return False
-	if jeedom_utils.ByteToHex(message.decode('hex')[0]) == "00":
-		logging.debug("Error: Packet first byte is 00")
-		return False
-	if not len(message.decode('hex')) > 1:
-		logging.debug("Error: Packet is not longer than one byte")
-		return False
-	cmd_len = int( jeedom_utils.ByteToHex( message.decode('hex')[2]),16 )
-	if not len(message.decode('hex')) == 8:
-		logging.debug("Error: Packet length is not valid : " + str(len(message.decode('hex'))))
-		logging.debug("Error: Packet length is not valid : " + str((cmd_len + 3)))
-		return False
-	return message
-
-########################
-## transmission des trames
-########################
-def plcbus_tx_command(message):
-	params = message.upper()
-	logging.debug("Params: " + params)
-
-	params_data = params.split("::")
-	
-	homeunit = params_data[0]
-	home = params_data[0][:1]
-	unit = params_data[0][1:]
-	
-	home = (jeedom_utils.StrToHex(home) - 65) * 16
-	unit = int(unit) - 1
-	homeunit = jeedom_utils.dec2hex(home + unit)[:-1].upper()
-		
-	command = hex(plcbus_command_to_hex[params_data[1]])[2:]
-	command = int(command, 16)
-
-	if ((command >= 28) or (command <= 1) or ((command >= 6) and (command <=9))):
-		homeunit = long(homeunit,16) & 240
-		homeunit = jeedom_utils.dec2hex(homeunit)[:-1].upper()
-		
-	if ((command == 28) or (command == 29)):
-		command += 32
-		
-	if (_phase == 3):
-		command += 64
-		
-	if (len(homeunit)<2):
-		homeunit = "0"+homeunit
-	else:
-		homeunit = str(homeunit)
-	
-	command = hex(command)[2:].upper()
-	if (len(str(command))<2):
-		command = "0"+str(command)
-	else:
-		command = str(command)
-
-	try:
-		params_data[2]
-	except NameError:
-		plcbus_data1 = "00"
-	else:
-		plcbus_data1 = int(str(params_data[2]),16)
-		plcbus_data1 = str(hex(plcbus_data1)[2:].upper())
-
-	try:
-		params_data[3]
-	except NameError:
-		plcbus_data2 = "00"
-	else:
-		plcbus_data2 = int(str(params_data[3]),16)
-		plcbus_data2 = str(hex(plcbus_data2)[2:].upper())
-
-	if (len(plcbus_data1)<2):
-		plcbus_data1 = "0"+plcbus_data1
-	else:
-		plcbus_data1 = str(plcbus_data1)
-		
-	if (len(plcbus_data2)<2):
-		plcbus_data2 = "0"+plcbus_data2
-	else:
-		plcbus_data2 = str(plcbus_data2)
-		
-	plcbus_frame = "02" + " " + "05" + " " + _usercode[2:] + " " + homeunit + " " + command + " " + plcbus_data1 + " " + plcbus_data2 + " " + "03"
-	logging.debug("FRAME : " + str(plcbus_frame))
-	return plcbus_frame
-	
-########################
-## reception des trames
-########################
-def plcbus_rx_status(homeunit, action, message):
-
-	status = jeedom_utils.StrToHex(message[4]) & 0x1F
-	if (action != None):
-		action = action & 0x1F
-	
-	# Si la reponse est une reponse directe de la commande emise
-	if ((homeunit != None) and (homeunit == jeedom_utils.StrToHex(message[3]))):
-		
-		# Si trois phases, attendre le rapport etat du coupleur
-		if (_phase==3):
-			if ((jeedom_utils.StrToHex(message[4]) == 0x0D) or (jeedom_utils.StrToHex(message[4]) == 0x0E) or (jeedom_utils.StrToHex(message[4]) >= 0x1E)):
-				return True
-		
-		# Si une commande de requete identification ID attend un signal de retour ID rxed
-		elif ((action == 0x1C) or (action == 0x1D)):
-			if (jeedom_utils.StrToHex(message[7]) == 0x40):
-				return True
-		
-		# Si une commande de requete (request or get) attend un rapport de succes PLCBUS autre unite
-		
-		elif ((action == 0x0F) or (action == 0x18) or (action == 0x19)):
-			if (jeedom_utils.StrToHex(message[7]) == 0x0C):
-				return True
-		
-		# Si une configuration ou effacement de scene ou une activation ou desactivation adresse de scene Unite 10 a 16 pour chaque Home envoyer un 1.
-		
-		elif ((action == 0x12) or (action == 0x13) or (action == 0x14) or (((action == 0x02) or (action == 0x03)) and ((jeedom_utils.StrToHex(message[3]) & 0x0F) >= 0x09))):
-			if (jeedom_utils.StrToHex(message[7]) == 0x1C):
-				return True
-		
-		# Toutes les autres commandes requierent seulement un ACK
-		elif (jeedom_utils.StrToHex(message[7]) == 0x20):
-			return True
-			
-	elif ((status == 0x0D) or (status == 0x0E) or ((homeunit != None) and (homeunit != jeedom_utils.StrToHex(message[3])))):
-		logging.debug("Detection du trafic initie par un autre emetteur")
-		plcbus_output_message(message)
-		return False
-		
-	elif (homeunit == None):
-		logging.debug("Detection du trafic initie par un autre emetteur")
-		return True
-		
-	else:
-		return False
-
-########################
-## validation des trames
-########################
-def plcbus_rx_valid_frame(message):
-	# Did we receive a valid 9 byte PLCBUS frame?
-	if (len(message) == 9):
-		logging.debug("Packet Received = " + jeedom_utils.ByteToHex(message))
-		
-		# STX is Frame Start Bit 02H, LENGTH is DATA length, DATA is Data Bit, and ETX is Frame End Bit 03H
-		if (((jeedom_utils.StrToHex(message[1]) == 0x06) and (jeedom_utils.StrToHex(message[0]) == 0x02) and (jeedom_utils.StrToHex(message[8]) == 0x03)) or ((sum(bytearray(message)) % 0x100) == 0x0)):
-			logging.debug("Valid Packet Received")
-			return True
-		else:
-			logging.debug("Invalid Packet Received")
-			return False
 
 ########################
 ## OUTPUT Message
 ########################
-def plcbus_output_message(message):
-	test = jeedom_utils.StrToHex(message[4]) & 0x1F
-	plcbus_status = plcbus_hex_to_command[test]
-	home = int(jeedom_utils.StrToHex(message[3]) / 16) + 65
-	home = struct.pack('l', home)
-	unit = jeedom_utils.StrToHex(message[3]) % 16 + 1
+def send_socket(message, num_ack):
+	message = message.split("::")
 	
 	action = {}
-	action['id'] = str(home)[:1] + str(unit)
-	action['housecode'] = str(home)[:1]
-	action['unitcode'] = str(unit)
-	action['command'] = str(plcbus_status)
-	action['data1'] = str(jeedom_utils.StrToHex(message[5]))
-	action['data2'] = str(jeedom_utils.StrToHex(message[6]))
+	action['id'] = str(message[0]).replace('\x00', '')
+	action['command'] = str(message[1]).replace('\x00', '')
+	action['data1'] = str(message[2]).replace('\x00', '')
+	action['data2'] = str(message[3]).replace('\x00', '')
+	action['num_ack'] = str(num_ack).replace('\x00', '')
 	
-	plcbus_status = str(action['id']+","+action['command']+","+action['data1']+","+action['data2'])
+	plcbus_statut = str(action['id']+","+action['command']+","+action['data1']+","+action['data2']+","+action['num_ack'])
 	logging.debug('Decode data : '+str(action))
 	try:
 		globals.JEEDOM_COM.add_changes('devices::'+action['id'],action)
 	except Exception, e:
 		pass
 	
-	return plcbus_status
+	return plcbus_statut
 
 # ----------------------------------------------------------------------------
 	
@@ -346,23 +316,21 @@ def read_plcbus():
 	message = None
 	try:
 		byte = jeedom_serial.read()
+		if byte:
+			if byte.encode('hex') == '02':
+				message = byte + jeedom_serial.readbytes(8)
+				message = str(message.encode('hex'))
+				logging.debug("Serial IN message brut : " + message)
+				if int(message[14]) > 1:
+					logging.debug("REPONSE ACK")
+				affiche_rx(None, None, message, int(message[14]))
+				
 	except Exception, e:
 		logging.error("Error in read_plcbus: " + str(e))
 		if str(e) == '[Errno 5] Input/output error':
 			logging.error("Exit 1 because this exeption is fatal")
 			shutdown()
-	try:
-		if byte:
-			message = byte + jeedom_serial.readbytes(8)
-			if plcbus_rx_valid_frame(message):
-				logging.debug("OUTPUT : " + str(plcbus_output_message(message)))
 
-			else:
-				logging.error("Error: Incoming packet not valid length (" + jeedom_utils.ByteToHex(message) + ")."  + str(len(message)))
-	except OSError, e:
-		logging.error("Error in read_plcbus on decode message : " + str(jeedom_utils.ByteToHex(message))+" => "+str(e))
-
-# ----------------------------------------------------------------------------
 
 def listen():
 	logging.debug("Start listening...")
@@ -373,7 +341,7 @@ def listen():
 	logging.debug("Start deamon")
 	try:
 		while 1:
-			time.sleep(0.02)
+			time.sleep(0.03)
 			read_plcbus()
 			read_socket()
 	except KeyboardInterrupt:
@@ -453,6 +421,9 @@ if args.serialrate:
 if args.cycle:
 	_cycle = float(args.cycle)
 
+if _usercode[:2] == '0x':
+	_usercode = _usercode[2:]
+	
 jeedom_utils.set_log_level(_log_level)
 
 logging.info('Start JeePlcBusd')
@@ -486,7 +457,6 @@ try:
 	if not globals.JEEDOM_COM.test():
 		logging.error('Network communication issues. Please fixe your Jeedom network configuration.')
 		shutdown()
-
 	jeedom_serial = jeedom_serial(device=_device,rate=_serial_rate,timeout=_serial_timeout,parity = 'N',stopbits = 1,bytesize = 8)
 	jeedom_socket = jeedom_socket(port=_socket_port,address=_socket_host)
 	listen()
