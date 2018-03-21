bf1ddc008912566322e0cee9f03e37fbce26268d
totoff974 <totoff974@users.noreply.github.com>
2018-03-21 16:28:11 +0400

    force la majuscule pour ID

--- core/class/JeePlcBus.class.php
+++ core/class/JeePlcBus.class.php
@@ -231,6 +231,7 @@ class JeePlcBus extends eqLogic {
 	}
 	
     public function preUpdate() {
+		$this->setLogicalId(strtoupper($this->getLogicalId()));
 		$home = array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P");
 		$unit = array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16");
 		$HU = $this->getLogicalId();
